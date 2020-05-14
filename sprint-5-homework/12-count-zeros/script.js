cefa48bdef4b7852366e7b17debce15e94709238
Nikita <kni0805@yandex.ru>
2020-05-14 17:57:26 +0300

    уменьшил число итераций в №12

--- sprint-5-homework/12-count-zeros/script.js
+++ sprint-5-homework/12-count-zeros/script.js
@@ -8,21 +8,20 @@
 
 function countZeros(n) {
     let result = 0;
-    let iterations = 0;
     for(let i = 10; i < n + 1; i++){
-        iterations++;
         if(i.toString().includes('0')){
             let current = i;
             while(current >= 10){
-                iterations++;
                 if(!(current % 10)){
                     result++;
                 }
                 current = Math.floor(current / 10);
             }
         }
+        else{
+            i += 8;
+        }
     }
-    console.log(`Iterations: ${iterations}`);
     return result;
 }
 
@@ -30,4 +29,3 @@ function countZeros(n) {
 
 console.log(countZeros(20)); // 2 – два нуля, по одному в числах 10 и 20
 console.log(countZeros(100)); // 11 – 11 нулей в числах: 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
-console.log(countZeros(10000)); // 64
