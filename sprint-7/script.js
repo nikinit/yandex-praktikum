425d4849effe9261e7ff02f8deaf563bc1860594
Nikita <kni0805@yandex.ru>
2020-08-22 15:34:39 +0300

    убрал копипасту, закрытие любого попапа выполняется через одну и ту же функцию

--- sprint-7/script.js
+++ sprint-7/script.js
@@ -47,6 +47,9 @@ const errorMessages = {
   requiredLink: 'Здесь должна быть ссылка'
 };
 
+const popupClasses = {
+  popupOpened: 'popup_is-opened',
+}
 const places = document.querySelector('.places-list');
 const editForm = document.forms.edit;
 const editPopup = document.getElementById('editPopup');
@@ -58,77 +61,69 @@ const addPopup = document.getElementById('addPopup');
 const openAddButton = document.querySelector('.user-info__add-button');
 const addButton = document.getElementById('addButton');
 const closeAddFormButton = document.getElementById('addClose');
+const imagePopup = document.getElementById('imagePopup');
+const forms = [...document.forms];
+const popups = [...document.querySelectorAll('.popup')];
 
 function createCard(placeValue, pictureValue){
-    const card = document.createElement('div');
-    const cardImage = document.createElement('div');
-    const deleteButton = document.createElement('button');
-    const cardDescription = document.createElement('div');
-    const cardName = document.createElement('h3');
-    const likeButton = document.createElement('button');
+  const card = document.createElement('div');
+  const cardImage = document.createElement('div');
+  const deleteButton = document.createElement('button');
+  const cardDescription = document.createElement('div');
+  const cardName = document.createElement('h3');
+  const likeButton = document.createElement('button');
 
-    card.classList.add('place-card');
+  card.classList.add('place-card');
 
-    cardImage.classList.add('place-card__image');
-    cardImage.style.backgroundImage = `url(${pictureValue})`;
-    deleteButton.classList.add('place-card__delete-icon');
-    cardImage.appendChild(deleteButton);
+  cardImage.classList.add('place-card__image');
+  cardImage.style.backgroundImage = `url(${pictureValue})`;
+  deleteButton.classList.add('place-card__delete-icon');
+  cardImage.appendChild(deleteButton);
 
-    cardDescription.classList.add('place-card__description');
-    cardName.classList.add('place-card__name');
-    cardName.textContent = placeValue;
-    likeButton.classList.add('place-card__like-icon');
-    cardDescription.appendChild(cardName);
-    cardDescription.appendChild(likeButton);
+  cardDescription.classList.add('place-card__description');
+  cardName.classList.add('place-card__name');
+  cardName.textContent = placeValue;
+  likeButton.classList.add('place-card__like-icon');
+  cardDescription.appendChild(cardName);
+  cardDescription.appendChild(likeButton);
 
-    card.appendChild(cardImage);
-    card.appendChild(cardDescription);
-
-    return card;
+  card.appendChild(cardImage);
+  card.appendChild(cardDescription);
+  card.addEventListener('click', likeCard);
+  card.addEventListener('click', deleteCard);
+  card.addEventListener('click', openImage);
+  return card;
 }
 
 function addCard(event){
-    event.preventDefault();
-    const place = addForm.elements.name;
-    const picture = addForm.elements.link;
-    const card = createCard(place.value, picture.value);
-
-    places.appendChild(card);
-    addForm.reset();
-    closeAddForm();
-}
-
-function openAddForm(event){
-  const placeError = document.getElementById('placeError');
-  const pictureError = document.getElementById('pictureError');
+  event.preventDefault();
   const place = addForm.elements.name;
   const picture = addForm.elements.link;
-  addPopup.classList.add('popup_is-opened');
+  const card = createCard(place.value, picture.value);
+  places.appendChild(card);
+  addForm.reset();
+  addPopup.classList.remove(popupClasses.popupOpened);
+}
+
+function openAddForm(){
+  const placeError = document.getElementById('placeError');
+  const pictureError = document.getElementById('pictureError');
+  addPopup.classList.add(popupClasses.popupOpened);
   addButton.setAttribute('disabled', true);
   placeError.textContent = '';
   pictureError.textContent = '';
-  place.setAttribute('style', 'border-color: none');
-  picture.setAttribute('style', 'border-color: none');
 }
 
-function closeAddForm(event){
-    addPopup.classList.remove('popup_is-opened');
-    addForm.reset();
-}
-
-function openEditForm(event){
+function openEditForm(){
   const currentName = document.querySelector('.user-info__name').textContent;
   const currentAbout = document.querySelector('.user-info__job').textContent;
   const name = editForm.elements.name;
   const about = editForm.elements.about;
   const nameError = document.getElementById('nameError');
   const aboutError = document.getElementById('aboutError');
-  editPopup.classList.add('popup_is-opened');
-  name.setAttribute('style', 'border-botom-color: none');
-  about.setAttribute('style', 'border-botom-color: none');
+  editPopup.classList.add(popupClasses.popupOpened);
   nameError.textContent = '';
   aboutError.textContent = '';
-  editButton.setAttribute('disabled', true);
   name.value = currentName;
   about.value = currentAbout;
 }
@@ -141,73 +136,45 @@ function editAuthor(event){
   const newAbout = document.querySelector('.user-info__job');
   newName.textContent = name;
   newAbout.textContent = about;
-  closeEditForm();
-}
-
-function closeEditForm(event){
-  editPopup.classList.remove('popup_is-opened');
-  editForm.reset();
+  editPopup.classList.remove(popupClasses.popupOpened);
 }
 
 function likeCard(event){
-    if(event.target.classList.contains('place-card__like-icon')){
-        event.target.classList.toggle('place-card__like-icon_liked');
-    }
+  if(event.target.classList.contains('place-card__like-icon')){
+      event.target.classList.toggle('place-card__like-icon_liked');
+  }
 }
 
 function deleteCard(event){
-    const card = event.path[2];
-    if(event.target.classList.contains('place-card__delete-icon')){
-        places.removeChild(card);
-    }
+  const card = event.path[2];
+  if(event.target.classList.contains('place-card__delete-icon')){
+      places.removeChild(card);
+  }
 }
 
 function openImage(event){
   if(event.target.classList.contains('place-card__image')){
-
-    const popupBox = document.createElement('div');
-    const imageBox = document.createElement('div');
-    const image = document.createElement('img');
     const imageAddress = event.target.style.backgroundImage;
-    const closeButton = document.createElement('div');
-
-    popupBox.classList.add('popup', 'popup_is-opened');
-    imageBox.style.background = imageAddress;
-    imageBox.style.position = 'relative';
-    imageBox.style.backgroundSize = 'cover';
-    image.classList.add('place-card__image_popup');
+    const imageBox = imagePopup.children[0];
+    const image = imageBox.children[0];
+    imagePopup.classList.add(popupClasses.popupOpened);
+    imageBox.style.backgroundImage = imageAddress;
     image.src = imageAddress.slice(5, -2);
-    image.alt = 'cool image';
-    closeButton.classList.add('popup__close');
-    closeButton.id = 'imageClose';
-
-    imageBox.appendChild(image);
-    imageBox.appendChild(closeButton);
-    popupBox.appendChild(imageBox);
-    document.body.appendChild(popupBox);
-
-    popupBox.addEventListener('click', closeImage);
   }
-  
-
-
 }
 
-function closeImage(event){
-  if(event.target.id === 'imageClose'){
-    const popupBox = document.querySelector('.popup_is-opened');
-    document.body.removeChild(popupBox);
+function closePopup(event){
+  if(event.target.id === event.target.id.split('Close')[0] + 'Close'){
+    const popup = event.target.closest('.popup');
+    popup.classList.remove(popupClasses.popupOpened);
   }
 }
 
 function checkInputValidity(element, errorMessage = ''){
-
   if(element.value.match(/\S/g) === null){
     errorMessage.textContent = errorMessages.required;
     element.setAttribute('style', 'border-bottom-color: red;');
-    if(element.name === 'name'){
-      element.setAttribute('style', 'margin-bottom: 0;');
-    }
+    
   }
 
   else if(element.name === 'link'){  //url required
@@ -216,8 +183,8 @@ function checkInputValidity(element, errorMessage = ''){
       const url = new URL(element.value);
       img.src = url.href;
       img.onerror = function(){
-        element.setAttribute('style', 'border-bottom-color: red;');
         errorMessage.textContent = errorMessages.requiredLink;
+        element.setAttribute('style', 'border-bottom-color: red;');
         }
       img.onload = function(){
         errorMessage.textContent = '';
@@ -225,28 +192,28 @@ function checkInputValidity(element, errorMessage = ''){
       }
   } catch(error){
     errorMessage.textContent = errorMessages.requiredLink;
+    element.setAttribute('style', 'border-bottom-color: red;');
     }
   }
 
   else if((element.value.match(/\S/g).length < 2) || (element.value.match(/\S/g).length > 30)){
     errorMessage.textContent = errorMessages.fromTo;
-    element.setAttribute('style', 'border-bottom-color: red; margin-bottom: 0');
+    element.setAttribute('style', 'border-bottom-color: red');
   }
 
   else{
     errorMessage.textContent = '';
     element.removeAttribute('style', 'border-bottom-color: red;');
-    
   }
 }
 
-function setSubmitButtonState(button, ...theFields){
+function setSubmitButtonState(button, inputs){
   let isDisabled = undefined;
-  for(let i = 0; i < theFields.length; i++){
-    if(theFields[i].name === 'link'){
+  for(let i = 0; i < inputs.length; i++){
+    if(inputs[i].name === 'link'){
       const img = new Image();
       try{
-        const url = new URL(theFields[i].value);
+        const url = new URL(inputs[i].value);
         img.src = url.href;
         img.onerror = function(){
           isDisabled = true;
@@ -260,11 +227,12 @@ function setSubmitButtonState(button, ...theFields){
         button.setAttribute('disabled', isDisabled);
         }
     }
-    else if ((theFields[i].value.match(/\S/g) === null) || (theFields[i].value.match(/\S/g).length < 2) || (theFields[i].value.match(/\S/g).length > 30)){
+
+    else if ((inputs[i].value.match(/\S/g) === null) || (inputs[i].value.match(/\S/g).length < 2) || (inputs[i].value.match(/\S/g).length > 30)){
       isDisabled = true;
       button.setAttribute('disabled', isDisabled);
-      
     }
+
     else{
       if(isDisabled === true){
         button.setAttribute('disabled', isDisabled);
@@ -276,36 +244,27 @@ function setSubmitButtonState(button, ...theFields){
   }
 }
 
-function setEventListeners(event){
-  const name = editForm.elements.name; //element
-  const nameError = document.getElementById('nameError'); //errorMessage
-  const about = editForm.elements.about;
-  const aboutError = document.getElementById('aboutError');
-  const place = addForm.elements.name;
-  const placeError = document.getElementById('placeError');
-  const picture = addForm.elements.link;
-  const pictureError = document.getElementById('pictureError');
-  setSubmitButtonState(editButton, name, about);
-  checkInputValidity(name, nameError);
-  checkInputValidity(about, aboutError);
-  setSubmitButtonState(addButton, place, picture);
-  checkInputValidity(place, placeError);
-  checkInputValidity(picture, pictureError);
+function checkFormInputs(form){
+  const inputs = form.querySelectorAll('.popup__input');
+  const errors = form.querySelectorAll('.popup__error');
+  const button = form.querySelector('.popup__button');
+  setSubmitButtonState(button, inputs);
+  for(let i = 0; i < inputs.length; i++){
+    checkInputValidity(inputs[i], errors[i]);
+  }
 }
 
 for(let i = 0; i < initialCards.length; i++){
     const card = createCard(initialCards[i].name, initialCards[i].link);
     places.appendChild(card);
 }
-
+forms.forEach(form =>{
+  form.addEventListener('input', () => checkFormInputs(form));
+})
+popups.forEach(popup => {
+  popup.addEventListener('click', closePopup);
+})
 openAddButton.addEventListener('click', openAddForm);
-addForm.addEventListener('input', setEventListeners);
-addForm.addEventListener('submit', addCard);
-closeAddFormButton.addEventListener('click', closeAddForm);
 openEditButton.addEventListener('click', openEditForm);
+addForm.addEventListener('submit', addCard);
 editForm.addEventListener('submit', editAuthor);
-editForm.addEventListener('input', setEventListeners);
-closeEditFormButton.addEventListener('click', closeEditForm);
-places.addEventListener('click', likeCard);
-places.addEventListener('click', deleteCard);
-places.addEventListener('click', openImage);
